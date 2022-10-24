3c9f3a59f6051a73ee5a4a9d21a9be6da5ba2a2c
Prseniakov Arsenii <presnyakov04@inbox.ru>
2022-10-24 20:17:51 +0300

    remade WsppCP

--- java-solutions/WsppCountPosition.java
+++ java-solutions/WsppCountPosition.java
@@ -10,13 +10,13 @@ public class WsppCountPosition {
         return words.entrySet().stream().sorted(new Comparator<Map.Entry<String, IntArray>>() {
             @Override
             public int compare(Map.Entry<String, IntArray> secondEntry, Map.Entry<String, IntArray> firstEntry) {
-                if (firstEntry.getValue().get(0) == secondEntry.getValue().get(0)) {
-                    if ((firstEntry.getValue().get(2) == secondEntry.getValue().get(2))) {
+                if (firstEntry.getValue().size() == secondEntry.getValue().size()) {
+                    if ((firstEntry.getValue().get(0) == secondEntry.getValue().get(0))) {
                         return secondEntry.getValue().get(1) - firstEntry.getValue().get(1);
                     }
-                    return secondEntry.getValue().get(2) - firstEntry.getValue().get(2);
+                    return secondEntry.getValue().get(0) - firstEntry.getValue().get(0);
                 }
-                return secondEntry.getValue().get(0) - firstEntry.getValue().get(0);
+                return secondEntry.getValue().size() - firstEntry.getValue().size();
             }
         }).toList();
     }
@@ -33,17 +33,9 @@ public class WsppCountPosition {
                         index = 1;
                     }
                     String word = text.nextWord().toLowerCase();
-                    if (words.containsKey(word)) {
-                        int count = words.get(word).get(0);
-                        words.get(word).append(index);
-                        words.get(word).append(line);
-                        words.get(text.nextWord().toLowerCase()).set(0, count + 1);
-                    } else {
-                        words.put(word, new IntArray(2));
-                        words.get(word).append(1);
-                        words.get(word).append(index);
-                        words.get(word).append(line);
-                        }
+                    words.putIfAbsent(word, new IntArray(2));
+                    words.get(word).append(line);
+                    words.get(word).append(index);
                     index++;
                     }
                 BufferedWriter writer = new BufferedWriter(new FileWriter(args[1], StandardCharsets.UTF_8));
@@ -51,9 +43,9 @@ public class WsppCountPosition {
                     List<Map.Entry<String, IntArray>> sortedWords = sortWords(words);
                     for (Map.Entry<String, IntArray> pair: sortedWords) {
                         writer.write(pair.getKey());
-                        writer.write(" " + pair.getValue().get(0));
-                        for (int i = 2; i < pair.getValue().size(); i+=2) {
-                            writer.write(" " + pair.getValue().get(i) + ":" + pair.getValue().get(i-1));
+                        writer.write(" " + pair.getValue().size()/2);
+                        for (int i = 0; i < pair.getValue().size(); i+=2) {
+                            writer.write(" " + pair.getValue().get(i) + ":" + pair.getValue().get(i+1));
                         }
                         writer.newLine();
                     }
