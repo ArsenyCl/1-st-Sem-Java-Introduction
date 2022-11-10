92011b8587d2e2ceddaaa7b48bbd9a6a93819c41
Prseniakov Arsenii <presnyakov04@inbox.ru>
2022-11-10 17:55:38 +0300

    remade

--- java-solutions/md2html/ACPair.java
+++ java-solutions/md2html/ACPair.java
@@ -1,25 +1,14 @@
 package md2html;
 
-public class ACPair {
-    private static int intValue;
-    private static String strValue;
-    ACPair(int intIn, String strIn) {
+public  class ACPair {
+    public int intValue;
+    public  String strValue;
+    public ACPair(int intIn, String strIn) {
         intValue = intIn;
         strValue = strIn;
     }
-    public static void setIntValue(int in) {
+    public void setIntValue(int in) {
         intValue = in;
     }
-    public static void setStrValue(String in) {
-        strValue = in;
-    }
-    public static int getIntValue() {
-        return intValue;
-    }
-    public static String getStrValue() {
-        return strValue;
-    }
-    public int hasCode() {
-         return 0;
-    }
 }
+
--- java-solutions/md2html/Md2Html.java
+++ java-solutions/md2html/Md2Html.java
@@ -1,141 +1,115 @@
 package md2html;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class Md2Html {
-    private static int strongZv = -1;
-    private static int strongPod = -1;
-    private static int emZv = -1;
-    private static int emPod = -1;
-    private static int s = -1;
-    private static int u = -1;
-    private static int code = -1;
-    private static void checkSymb(String in, ArrayList<String> list) {
-        if (in.equals("**")) {
-            if (strongZv > -1) {
-                list.set(strongZv, "<strong>");
-                list.set(list.size() - 1, "</strong>");
-                strongZv = -1;
-            } else {
-                strongZv = list.size() - 1;
-            }
-        } else if (in.equals("__")) {
-            if (strongPod > -1) {
-                list.set(strongPod, "<strong>");
-                list.set(list.size() - 1, "</strong>");
-                strongPod = -1;
-            } else {
-                strongPod = list.size() - 1;
-            }
-        } else if (in.equals("*")) {
-            if (emZv > -1) {
-                list.set(emZv, "<em>");
-                list.set(list.size() - 1, "</em>");
-                emZv = -1;
-            } else {
-                emZv = list.size() - 1;
-            }
-        } else if (in.equals("_")) {
-            if (emPod > -1) {
-                list.set(emPod, "<em>");
-                list.set(list.size() - 1, "</em>");
-                emPod = -1;
-            } else {
-                emPod = list.size() - 1;
-            }
-        } else if(in.equals("--")) {
-            if (s > -1) {
-                list.set(s, "<s>");
-                list.set(list.size()-1, "</s>");
-                s = -1;
-            } else {
-                s = list.size()-1;
-            }
-        } else if(in.equals("`")) {
-            if (code > -1) {
-                list.set(code, "<code>");
-                list.set(list.size() - 1, "</code>");
-                code = -1;
-            } else {
-                code = list.size() - 1;
-            }
-        } else if(in.equals("++")){
+        public static HashMap<String, ACPair> subs = new HashMap<String, ACPair>();
+        public static HashMap<String, String> change = new HashMap<String, String>();
 
-            if (u > -1) {
-                list.set(u, "<u>");
-                list.set(list.size() - 1, "</u>");
-                u = -1;
-            } else {
-                u = list.size() - 1;
-            }
-        } else if (in.equals("<")) {
-            list.set(list.size()-1, "&lt;");
-        } else if (in.equals(">")) {
-            list.set(list.size()-1, "&gt;");
-        } else if (in.equals("&")) {
-            list.set(list.size()-1, "&amp;");
-        } else if(in.equals("\\*")) {
-            list.set(list.size()-1, "*");
-        } else if (in.equals("\\_")) {
-            list.set(list.size() - 1, "_");
-        } else if(in.equals("\\++")) {
-            list.set(list.size() - 1, "++");
-        } else if(in.equals("\\+")) {
-            list.set(list.size() - 1, "+");
-        }
-    }
-    public static void main(String[] args) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(args[1], StandardCharsets.UTF_8))) {
-            SpecialScanner in = new SpecialScanner(new File(args[0]), StandardCharsets.UTF_8);
-            try {
-                while(in.hasNext()) {
-                    ArrayList<String> out = new ArrayList<>();
-                    String head;
-                    if (in.next().charAt(0) == '#' && in.next().charAt(in.next().length()-1) == ' ') {
-                         head = "h" + (in.next().length()-1) + ">";
-                         out.add("<"+head);
-                    } else {
-                        head = "p>";
-                        out.add("<" + head);
-                        if (in.next().charAt(0)=='\n') {
-                            continue;
+        public static void main(String[] args)  {
+            change.put("<", "&lt;");
+            change.put(">", "&gt;");
+            change.put("&", "&amp;");
+            change.put("\\*", "*");
+            change.put("\\_", "_;");
+            change.put("\\++", "++");
+            change.put("\\+", "+");
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(args[1], StandardCharsets.UTF_8))) {
+                SpecialScanner in = new SpecialScanner(new File(args[0]), StandardCharsets.UTF_8);
+                try {
+                    while(in.hasNext()) {
+                        ACPair strongZv = new ACPair(-1, "strong>");
+                        ACPair strongPod = new  ACPair(-1, "strong>");
+                        ACPair emZv = new  ACPair(-1, "em>");
+                        ACPair emPod = new  ACPair(-1, "em>");
+                        ACPair s = new  ACPair(-1, "s>");
+                        ACPair code = new  ACPair(-1, "code>");
+                        ACPair u = new ACPair(-1, "u>");
+                        subs.put("**", strongZv);
+                        subs.put("__", strongPod);
+                        subs.put("*", emZv);
+                        subs.put("`", code);
+                        subs.put("_", emPod);
+                        subs.put("--", s);
+                        subs.put("++", u);
+                        ArrayList<String> out = new ArrayList<>();
+                        String head;
+                        if (in.next().charAt(0) == '#' && in.next().charAt(in.next().length() - 1) == ' ') {
+                            head = "h" + (in.next().length() - 1) + ">";
+                            out.add("<" + head);
                         } else {
-                            out.add(in.next());
-                            checkSymb(in.next(), out);
+                            head = "p>";
+                            out.add("<" + head);
+                            if (in.next().charAt(0) == '\n') {
+                                continue;
+                            } else {
+                                out.add(change.getOrDefault(in.next(), in.next()));
+                                if (subs.containsKey(in.next())) {
+                                    int index = subs.get(in.next()).intValue;
+                                    String sub = subs.get(in.next()).strValue;
+                                    if (index > -1) {
+                                        out.set(index, "<" + sub);
+                                        out.set(out.size() - 1, "</" + sub);
+                                        subs.get(in.next()).setIntValue(-1);
+                                    } else {
+                                        subs.get(in.next()).setIntValue(out.size() - 1);
+                                    }
+                                }
+                            }
                         }
-                    }
-                    while (in.hasNext()) {
-                        if (in.skipLines) {
-                            break;
-                        } else {
-                            out.add(in.next());
-                            checkSymb(in.next(), out);
+                        while (in.hasNext()) {
+                            if (in.skipLines) {
+                                break;
+                            } else {
+                                out.add(change.getOrDefault(in.next(), in.next()));
+                                if (subs.containsKey(in.next())) {
+                                    int index = subs.get(in.next()).intValue;
+                                    String sub = subs.get(in.next()).strValue;
+                                    if (index > -1) {
+                                        out.set(index, "<" + sub);
+                                        out.set(out.size() - 1, "</" + sub);
+                                        subs.get(in.next()).setIntValue(-1);
+                                    } else {
+                                        subs.get(in.next()).setIntValue(out.size() - 1);
+                                    }
+                                }
+                            }
                         }
-
+                        if (out.get(out.size() - 1).charAt(0) == '\n') {
+                            out.remove(out.size() - 1);
+                        }
+                        out.add("</" + head);
+                        for (String each : out) {
+                            writer.write(each);
+                        }
+                        writer.newLine();
                     }
-                    if (out.get(out.size()-1).charAt(0) == '\n') {
-                        out.remove(out.size()-1);
-                    }
-                    out.add("</" + head);
-                    for (String each: out) {
-                        writer.write(each);
-                    }
-                    writer.newLine();
-                    emZv = -1;
-                    emPod = -1;
-                    strongZv = -1;
-                    strongPod = -1;
-                    s = -1;
-                    code = -1;
+                } catch (IOException e){
+                    System.out.println(e.getLocalizedMessage());
+                } finally {
+                    in.close();
                 }
-            } finally {
-                in.close();
-            }
 
-        } catch(IOException e) {
-            System.out.println(e.getLocalizedMessage());
+            } catch(IOException e) {
+                System.out.println(e.getLocalizedMessage());
+            }
+        }
+    public static class ACPair {
+        public int intValue;
+        public  String strValue;
+        public ACPair(int intIn, String strIn) {
+            intValue = intIn;
+            strValue = strIn;
+        }
+        public void setIntValue(int in) {
+            intValue = in;
         }
     }
 }
