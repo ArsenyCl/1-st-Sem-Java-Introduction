478b36b86b4f1a6ac8e38580968adc56d9a9a337
Michael Ruzavin <misharuz25@gmail.com>
2022-11-10 17:22:18 +0300

    Checked by an instructor

--- java-solutions/md2html/Md2Html.java
+++ java-solutions/md2html/Md2Html.java
@@ -13,77 +13,92 @@ public class Md2Html {
     private static int u = -1;
     private static int code = -1;
     private static void checkSymb(String in, ArrayList<String> list) {
-        if (in.equals("**")) {
-            if (strongZv > -1) {
-                list.set(strongZv, "<strong>");
-                list.set(list.size() - 1, "</strong>");
-                strongZv = -1;
-            } else {
-                strongZv = list.size() - 1;
-            }
-        } else if (in.equals("__")) {
-            if (strongPod > -1) {
-                list.set(strongPod, "<strong>");
-                list.set(list.size() - 1, "</strong>");
-                strongPod = -1;
-            } else {
-                strongPod = list.size() - 1;
-            }
-        } else if (in.equals("*")) {
-            if (emZv > -1) {
-                list.set(emZv, "<em>");
-                list.set(list.size() - 1, "</em>");
-                emZv = -1;
-            } else {
-                emZv = list.size() - 1;
-            }
-        } else if (in.equals("_")) {
-            if (emPod > -1) {
-                list.set(emPod, "<em>");
-                list.set(list.size() - 1, "</em>");
-                emPod = -1;
-            } else {
-                emPod = list.size() - 1;
-            }
-        } else if(in.equals("--")) {
-            if (s > -1) {
-                list.set(s, "<s>");
-                list.set(list.size()-1, "</s>");
-                s = -1;
-            } else {
-                s = list.size()-1;
-            }
-        } else if(in.equals("`")) {
-            if (code > -1) {
-                list.set(code, "<code>");
-                list.set(list.size() - 1, "</code>");
-                code = -1;
-            } else {
-                code = list.size() - 1;
-            }
-        } else if(in.equals("++")){
+        switch (in) {
+            case "**":
+                if (strongZv > -1) {
+                    list.set(strongZv, "<strong>");
+                    list.set(list.size() - 1, "</strong>");
+                    strongZv = -1;
+                } else {
+                    strongZv = list.size() - 1;
+                }
+                break;
+            case "__":
+                if (strongPod > -1) {
+                    list.set(strongPod, "<strong>");
+                    list.set(list.size() - 1, "</strong>");
+                    strongPod = -1;
+                } else {
+                    strongPod = list.size() - 1;
+                }
+                break;
+            case "*":
+                if (emZv > -1) {
+                    list.set(emZv, "<em>");
+                    list.set(list.size() - 1, "</em>");
+                    emZv = -1;
+                } else {
+                    emZv = list.size() - 1;
+                }
+                break;
+            case "_":
+                if (emPod > -1) {
+                    list.set(emPod, "<em>");
+                    list.set(list.size() - 1, "</em>");
+                    emPod = -1;
+                } else {
+                    emPod = list.size() - 1;
+                }
+                break;
+            case "--":
+                if (s > -1) {
+                    list.set(s, "<s>");
+                    list.set(list.size() - 1, "</s>");
+                    s = -1;
+                } else {
+                    s = list.size() - 1;
+                }
+                break;
+            case "`":
+                if (code > -1) {
+                    list.set(code, "<code>");
+                    list.set(list.size() - 1, "</code>");
+                    code = -1;
+                } else {
+                    code = list.size() - 1;
+                }
+                break;
+            case "++":
 
-            if (u > -1) {
-                list.set(u, "<u>");
-                list.set(list.size() - 1, "</u>");
-                u = -1;
-            } else {
-                u = list.size() - 1;
-            }
-        } else if (in.equals("<")) {
-            list.set(list.size()-1, "&lt;");
-        } else if (in.equals(">")) {
-            list.set(list.size()-1, "&gt;");
-        } else if (in.equals("&")) {
-            list.set(list.size()-1, "&amp;");
-        } else if(in.equals("\\*")) {
-            list.set(list.size()-1, "*");
-        } else if (in.equals("\\_")) {
-            list.set(list.size() - 1, "_");
-        } else if(in.equals("\\++")) {
-            list.set(list.size() - 1, "++");
-        } else if(in.equals("\\+")) {
-            list.set(list.size() - 1, "+");
+                if (u > -1) {
+                    list.set(u, "<u>");
+                    list.set(list.size() - 1, "</u>");
+                    u = -1;
+                } else {
+                    u = list.size() - 1;
+                }
+                break;
+            case "<":
+                list.set(list.size() - 1, "&lt;");
+                break;
+            case ">":
+                list.set(list.size() - 1, "&gt;");
+                break;
+            case "&":
+                list.set(list.size() - 1, "&amp;");
+                break;
+            case "\\*":
+                list.set(list.size() - 1, "*");
+                break;
+            case "\\_":
+                list.set(list.size() - 1, "_");
+                break;
+            case "\\++":
+                list.set(list.size() - 1, "++");
+                break;
+            case "\\+":
+                list.set(list.size() - 1, "+");
+                break;
         }
     }
     public static void main(String[] args) {
